5159bfae8b51cbd217520182a66a3e0ee065d6ff
charlesonunze <charlesonunze2@gmail.com>
2021-03-01 08:40:21 +0100

    feat: init commit

---
+++ payment/procedural/check.go
@@ -0,0 +1,16 @@
+package procedural
+
+type CheckingAccount struct {
+	AccountOwner  string
+	RoutingNumber string
+	AccountNumber string
+	Balance       float32
+}
+
+func PayWithCheck(account *CheckingAccount, amount float32) bool {
+	if account.Balance > amount {
+		account.Balance -= amount
+		return true
+	}
+	return false
+}
